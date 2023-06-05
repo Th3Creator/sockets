2dc7bb8979dfb0b72c66324675f673bd049d04b7
Christian <christianlouzada.castro@gmail.com>
2023-06-04 21:25:32 -0300

    v1

---
+++ tcp/client.py
@@ -0,0 +1,28 @@
+import socket
+
+socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+
+host = 'localhost' # ip onde o servidor está... no caso é tudo na minha máquina local
+port = 7777 # tem que ser a mesma porta
+bufferSize = 1024 # buffer para a recepção de dados
+
+socket.connect(( host, port ))
+# vai fazer a conexão com o ip da máquina passada e qual porta vai se comunicar
+
+print("Conectando...")
+
+
+
+"""
+    Toda essa parte acima é padrão... 
+
+    O que vem após a conexão é o que o professor quer, quer seria:
+        
+        - pedir o arquivo pro servidor
+        - receber esse arquivo
+        - imprimir o tempo gasto desse envio
+        - imprimir a quantidade total de linhas desse arquivo
+        - notificar ao servidor se quer mais um arquivo ou pode fechar conexão
+"""
+
+# https://pythontic.com/modules/socket/introduction
---
+++ tcp/server.py
@@ -0,0 +1,36 @@
+import socket 
+# faz a importação do módulo socket que vai dar acesso a todas às funções para realizar a comunicação 
+
+socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+# invocando o método socket que chama o construtor dela passando dois parâmetros:
+# (família de protocologo, tipo de protocologo (SOCK_STREM = tcp; SOCK_DGRAM = udp)))
+
+host = 'localhost' # o ip onde toda comunicação vai acontecer, coloquei localhost porquê vai ser tudo na minha máquina
+port = 7777 # a porta onde toda comunicação entre o cliente/servidor vai acontecer
+bufferSize = 1024 # buffer para a recepção de dados 
+
+socket.bind(( host, port ))
+# o método bind é responsável por associar uma porta a um host, no caso eu associei a porta 7777 a minha máquina local
+
+socket.listen()
+print("aguardando conexão...")
+# transforma o servidor em modo de escuta, ou seja, tá esperando que alguém faça uma comunicação
+
+connection, address = socket.accept()
+# quando o cliente fizer a conexão, ele automáticamente vai aceitar e retornar esses dois parâmetros
+# conexão com o cliente, endereço do cliente
+
+print("\nconectado com sucesso!")
+
+
+
+"""
+    Toda essa parte acima é padrão... 
+
+    O que vem após a conexão é o que o professor quer, quer seria:
+        
+        - lê o arquivo que o cliente pediu
+        - informar ao cliente o tempo gasto no envio do arquivo
+"""
+
+# https://pythontic.com/modules/socket/introduction
