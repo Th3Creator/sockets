37077542493133ac168536e99c81cef6d0128347
Christian <christianlouzada.castro@gmail.com>
2023-06-05 16:27:02 -0300

    v1

--- tcp/client.py
+++ tcp/client.py
@@ -2,14 +2,24 @@ import socket # https://pythontic.com/modules/socket/introduction
 
 client = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 
-host = 'localhost' # ip onde o servidor está... no caso é tudo na minha máquina local
-port = 7777 # tem que ser a mesma porta
-bufferSize = 1024 # buffer para a recepção de dados
+"""
 
+    host: ip onde o servidor está... no caso é tudo na minha máquina local
+    port: tem que ser a mesma porta
+    bufferSize: buffer para a recepção de dados
+
+"""
+host = 'localhost' 
+port = 7777 
+bufferSize = 100000000 
+
+"""
+
+    vai fazer a conexão com o ip da máquina passada e qual porta vai se comunicar
+
+"""
 client.connect(( host, port ))
-# vai fazer a conexão com o ip da máquina passada e qual porta vai se comunicar
-
-print("Conectando...")
+print("conectando!")
 
 """
     Toda essa parte acima é padrão... 
@@ -17,10 +27,47 @@ print("Conectando...")
     O que vem após a conexão é o que o professor quer, quer seria:
         
         - pedir o arquivo pro servidor
-        - receber esse arquivo
+        - receber esse arquivo e "ser remontados e salvos como arquivos de texto cliente"*
         - imprimir o tempo gasto desse envio
         - imprimir a quantidade total de linhas desse arquivo
         - notificar ao servidor se quer mais um arquivo ou pode fechar conexão
 """
 
-# tratamento de exceção 
+
+
+
+
+
+# tratamento de exceção aqui...
+# tentando dar uma adiantada:
+
+"""
+
+    nameFile: ler o arquivo que deseja que o servidor te mande
+    escolquer o nome do arquivo que deseja visualizar do servidor
+
+"""
+nameFile = str(input('qual arquivo deseja pedir?')) # 
+
+"""
+
+    transforma o str em bytes para poder ser transmitido
+    encode(): faz o processo de codificação, transformando os dados em bytes para poder ser transmitidos
+
+"""
+client.send( nameFile.encode() ) # 
+
+
+# preparar para receber os dados do servidor após requesição
+with open( nameFile, 'wb') as file:
+    while 1:
+        data = client.recv( bufferSize )
+
+        if not data:
+            break
+
+        file.write( data )
+
+        # saber quando o servidor vai parar de enviar os dados
+
+print("tudo recebido...")
--- tcp/server.py
+++ tcp/server.py
@@ -1,25 +1,47 @@
 import socket # https://pythontic.com/modules/socket/introduction
-# faz a importação do módulo socket que vai dar acesso a todas às funções para realizar a comunicação 
 
+"""
+
+    socket.socket(): invocando o método socket que chama o construtor dela passando dois parâmetros:
+    (família de protocologo, tipo de protocologo (SOCK_STREM = tcp; SOCK_DGRAM = udp))
+
+"""
 server = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-# invocando o método socket que chama o construtor dela passando dois parâmetros:
-# (família de protocologo, tipo de protocologo (SOCK_STREM = tcp; SOCK_DGRAM = udp)))
 
-host = 'localhost' # o ip onde toda comunicação vai acontecer, coloquei localhost porquê vai ser tudo na minha máquina
-port = 7777 # a porta onde toda comunicação entre o cliente/servidor vai acontecer
-bufferSize = 1024 # buffer para a recepção de dados 
+"""
 
+    host: o ip onde toda comunicação vai acontecer, coloquei localhost porquê vai ser tudo na minha máquina
+    port: a porta onde toda comunicação entre o cliente/servidor vai acontecer
+    bufferSize: buffer para a recepção de dados 
+
+"""
+host = 'localhost'  
+port = 7777 
+bufferSize = 1024 
+
+"""
+
+    bind(): o método bind é responsável por associar uma porta a um host, no caso eu associei a porta 7777 a minha máquina local
+
+"""
 server.bind(( host, port ))
-# o método bind é responsável por associar uma porta a um host, no caso eu associei a porta 7777 a minha máquina local
 
+"""
+
+    listen(): transforma o servidor em modo de escuta, ou seja, tá esperando que alguém faça uma comunicação
+
+"""
 server.listen()
 print("aguardando conexão...")
-# transforma o servidor em modo de escuta, ou seja, tá esperando que alguém faça uma comunicação
 
+
+"""
+
+    connection: a conexão com o cliente
+    address: endereço do cliente conectado
+
+"""
 connection, address = server.accept()
-# quando o cliente fizer a conexão, ele automáticamente vai aceitar e retornar esses dois parâmetros
-# conexão com o cliente, endereço do cliente
-
 print("\nconectado com sucesso!")
 
 """
@@ -28,7 +50,39 @@ print("\nconectado com sucesso!")
     O que vem após a conexão é o que o professor quer, quer seria:
         
         - lê o arquivo que o cliente pediu
+        - a transmissão deverá ser feita linha a linha*
         - informar ao cliente o tempo gasto no envio do arquivo
 """
 
-# tratamento de exceção 
+
+
+
+
+
+# tratamento de exceção aqui...
+# tentando dar uma adiantada:
+
+"""
+
+    nameFile: o nome do arquivo que o cliente pediu para o servidor
+    bufferSize: vai ser quantos bytes você quer receber, o n° 1024 já é o suficente
+    decode(): o decode é pra transformar os bytes em string, isso se dá porque toda vez que você envia algo na rede,
+    é necessário que você transforma aquele dado em bytes e quando chega é necessário fazer o processo inverso para 
+    poder visualizar o que foi enviado, basicamente transforma bytes em string
+
+"""
+nameFile = connection.recv( bufferSize ).decode() #  
+
+"""
+
+    with open(): essa função abre o arquivo e garante após isso que ele seja fechado
+    for data in file: lê esse arquivo e envia esse arquivo para o cliente
+    readlines: vai ler todo o arquivo e enviar para o cliente
+    
+"""
+with open( nameFile, 'rb' ) as file: # , 
+     
+    for data in file.readlines(): # 
+        connection.send( data )
+
+    print("arquivo enviado!")
