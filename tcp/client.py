11e2324ca24023b0e94166aebd4cac263ce575ac
Felipe <115735627+FelpzzzEX@users.noreply.github.com>
2023-06-19 21:51:26 -0300

    client.py

--- tcp/client.py
+++ tcp/client.py
@@ -5,43 +5,6 @@ host = 'localhost'
 port = 7777
 bufferSize = 100000000
 
-def receber_arquivos(client):
-    try:
-        nome_arquivo = input("Qual arquivo você deseja solicitar? ")
-
-        client.send(nome_arquivo.encode())
-
-        folderPath = "files_received/"
-
-        with open(folderPath + nome_arquivo, 'w') as arquivo:
-            start_time = time.time()
-
-            while True:
-                data = client.recv(bufferSize).decode()
-                if not data:
-                    break
-
-                arquivo.write(data)
-
-            end_time = time.time()
-            tempo_gasto = end_time - start_time
-
-        print("Arquivo recebido com sucesso.")
-        print("Tempo gasto:", tempo_gasto, "segundos")
-
-        with open(folderPath + nome_arquivo, 'r') as arquivo:
-            for linha in arquivo:
-                print(linha)
-
-        exibir_menu()
-
-    except Exception as e:
-        print("Ocorreu um erro durante o recebimento do arquivo:", str(e))
-
-def sair():
-    print("\nEncerrando o programa.")
-    raise SystemExit
-
 def exibir_menu():
     try:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -82,8 +45,45 @@ def exibir_menu_solicitacao():
     except Exception as e:
         print("Ocorreu um erro durante a conexão:", str(e))
 
+def receber_arquivos(client):
+    try:
+        nome_arquivo = input("Qual arquivo você deseja solicitar? ")
+
+        client.send(nome_arquivo.encode())
+
+        folderPath = "files_received/"
+
+        with open(folderPath + nome_arquivo, 'w') as arquivo:
+            start_time = time.time()
+
+            while True:
+                data = client.recv(bufferSize).decode()
+                if not data:
+                    break
+
+                arquivo.write(data)
+
+            end_time = time.time()
+            tempo_gasto = end_time - start_time
+
+        print("Arquivo recebido com sucesso.")
+        print("Tempo gasto:", tempo_gasto, "segundos")
+
+        with open(folderPath + nome_arquivo, 'r') as arquivo:
+            for linha in arquivo:
+                print(linha)
+
+        exibir_menu()
+
+    except Exception as e:
+        print("Ocorreu um erro durante o recebimento do arquivo:", str(e))
+
 def solicitar_tabela_arquivos():
     print("Função para solicitar a tabela de arquivos disponíveis.")
+
+def sair():
+    print("\nEncerrando o programa.")
+    raise SystemExit
 
 if __name__ == '__main__':
     exibir_menu()
